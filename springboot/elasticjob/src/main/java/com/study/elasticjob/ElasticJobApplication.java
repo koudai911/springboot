4ed934610899f76de4f4fd92960d480301c690fa
luoshangcai <1421976223@qq.com>
2020-04-25 17:20:38 +0800

    canal

---
+++ springboot/elasticjob/src/main/java/com/study/elasticjob/ElasticJobApplication.java
@@ -0,0 +1,13 @@
+package com.study.elasticjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJobApplication.class, args);
+    }
+
+}
---
+++ springboot/elasticjob/src/test/java/com/study/elasticjob/ElasticjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.elasticjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
