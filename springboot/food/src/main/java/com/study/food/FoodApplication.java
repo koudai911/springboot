6638d64037b4e1d94a8c2148924b96117c27a41c
lsc <1421976223@qq.com>
2020-09-13 20:34:19 +0800

    吃饭

--- springboot/food/src/main/java/com/study/food/FoodApplication.java
+++ springboot/food/src/main/java/com/study/food/FoodApplication.java
@@ -3,13 +3,27 @@ package com.study.food;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class FoodApplication {
+public class FoodApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(FoodApplication.class, args);
     }
 
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")//项目中的所有接口都支持跨域
+                .allowedOrigins("*")//所有地址都可以访问，也可以配置具体地址
+                .allowCredentials(true) //是否允许请求带有验证信息
+                .allowedMethods("*")//"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"
+                .allowedHeaders("*").maxAge(3600);// 跨域允许时间
+    }
 }
