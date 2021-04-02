269795e300cec6f0e66ee356ebcd4c6dc1a09b86
shancai.luo <1421976223@qq.com>
2021-04-02 14:21:18 +0800

    git 最新提交

--- springboot/edu_base/base_gateway/src/main/java/com/study/base/gateway/GatewayApplication.java
+++ springboot/edu_base/base_gateway/src/main/java/com/study/base/gateway/GatewayApplication.java
@@ -3,9 +3,17 @@ package com.study.base.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableZuulServer
 @EnableEurekaClient
+@EnableZuulProxy
+@EnableRetry
+@EnableHystrix
 public class GatewayApplication {
 
     public static void main(String[] args) {
